4f2c41df44bae399d7634f3168d92064d22cb3ff
adamjpatterson <adam@farar.net>
2024-03-09 16:35:18 -0500

    more

--- src/neuralplex/network.py
+++ src/neuralplex/network.py
@@ -28,6 +28,7 @@ class Network:
         # Activation Stage
         for i in range(0, len(X_train)):
             self.input_layer.neurons[i].activate(X_train[i])
+            
         # "Backpropagation" Stage
         for i in range(0, len(y_train)):
             y_train = y_train[i]
