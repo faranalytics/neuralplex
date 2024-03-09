35608e45bd1e091643ae6ec2af94e2a6ef2186a8
adamjpatterson <adam@farar.net>
2024-03-09 16:38:21 -0500

    more

--- src/neuralplex/utils.py
+++ src/neuralplex/utils.py
@@ -8,7 +8,7 @@ def get_edge_data(network: Network):
         for neuron1 in layer.neurons:
             if len(neuron1.neuronsRHS) != 0:
                 for neuron2 in neuron1.neuronsRHS:
-                    records.append({'source':neuron1.name, 'target':neuron2.name, 'weight':neuron1.m})
+                    records.append({"source": neuron1.name, "target": neuron2.name, "weight": neuron1.m})
             else:
-                records.append({'source':neuron1.name, 'target':None, 'weight':neuron1.m})
+                records.append({"source": neuron1.name, "target": None, "weight": neuron1.m})
     return records
