b589dd1bbe89415a80b116a646db54681d4f582c
adamjpatterson <adam@farar.net>
2024-03-11 17:40:31 -0400

    more

--- src/neuralplex/neuron.py
+++ src/neuralplex/neuron.py
@@ -57,7 +57,7 @@ class Neuron:
             self.value = self.m * sum(self.activation.values())
             for neuron in self.neuronsRHS:
                 neuron.activate(self.value, self)
-                # Under what conditions should an activation take place?
+                # Under what conditions should an activation value be propagated?
             self.activation_count = 0
 
     def propagate(self, error: float, neuron: Self = None) -> None:
